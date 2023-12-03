d2158f15fb18c27ed275f3d6eacaa03af1360a51
Sayantan Sikdar <sayantansikdar@Sayantans-MacBook-Pro.local>
2023-12-03 17:20:45 +0530

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,53 @@
+import React from "react"
+import "./style.css"
+import { Col, Container, Row } from "react-bootstrap"
+
+const Footer = () => {
+  return (
+    <footer>
+        <Container>
+          <Row className="footer-row">
+            <Col md={3} sm={5} className='box'>
+              <div className="logo">
+                  <ion-icon name="bag"></ion-icon>
+                  <h1>Rhymo</h1>
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor libero id et, in gravida. Sit diam duis mauris nulla cursus. Erat et lectus vel ut sollicitudin elit at amet.</p>
+            </Col>
+            <Col md={3} sm={5} className='box'>
+              <h2>About Us</h2>
+              <ul>
+                <li>Careers</li>
+                <li>Our Stores</li>
+                <li>Our Cares</li>
+                <li>Terms & Conditions</li>
+                <li>Privacy Policy</li>
+              </ul>
+            </Col>
+            <Col md={3} sm={5} className='box'>
+              <h2>Customer Care</h2>
+              <ul>
+                <li>Help Center </li>
+                <li>How to Buy </li>
+                <li>Track Your Order </li>
+                <li>Corporate & Bulk Purchasing </li>
+                <li>Returns & Refunds </li>
+              </ul>
+            </Col>
+            <Col md={3} sm={5} className='box'>
+              <h2>Contact Us</h2>
+              <ul>
+                <li>Haldia Institute of Technology, Haldia, West Bengal </li>
+                <li>sayantan314.hitcseaiml2020@gmail.com</li>
+                <li>brahmashnu545.hitcseaiml2020@gmail.com</li>
+                <li>Phone1: +91 8902091002</li>
+                <li>Phone2: +91 7595808098</li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+    </footer>
+  )
+}
+
+export default Footer
